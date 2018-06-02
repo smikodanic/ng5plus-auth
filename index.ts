fd45801d1c71d3b20172f63ad97913d17d6ff2ae
Sasa M <smikodanic@gmail.com>
2018-06-02 13:34:36 +0200

    ng5plus-auth init

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { AuthService } from './auth.service';
+export { IsLoggedService } from './routeGuards/isLogged.service';
+export { HasRoleService } from './routeGuards/hasRole.service';
+export { AutologinService } from './routeGuards/autoLogin.service';
